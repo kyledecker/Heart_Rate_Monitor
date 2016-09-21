0f170d91af3e401306434fbd58033d4940f0f5e0
Kyle Decker <kyle.decker@duke.edu>
2016-09-21 17:03:54 -0400

    Working on the main function to tie together sub-functions

--- heart_rate_monitor.py
+++ heart_rate_monitor.py
@@ -1,31 +1,43 @@
 
-if len(sys.argv)<2:
-    print('Please specify a filename')
-    sys.exit()
-else:
-    filename = sys.argv[1]
-    print('Analyzing the heart rate of data contained in: %s ...' %filename
-
 if __name__ == "__main__":
-    # First attain necessary info (fs, and size) from data                                                                                                                                                                      
+    
+    if (len(sys.argv)<2):
+        print('Please specify a filename')
+        sys.exit()
+    else:
+        filename = sys.argv[1]
+        print('Analyzing the heart rate of data contained in: %s ...' %filename)
+
+    # First attain necessary info (fs and size) from data                                                                                                                                              
     data_info = read_binary(filename,offset=0,count_read=1,init_flag=1)
     fs = data_info[0]
     file_size = data_info[1]
 
-    # Read in 10 seconds of data at a time until end of file
+    # Define Amount of time to read in at once (10 seconds)
+    time_var = 10
+    num_samples = fs*time_var
+    sample_size = 2*2 #2 bytes per sample assuming uint16, 2 samples (1 ECG, 1 PP)
     
-    # Take in 10 seconds of ECG and PP data at a time, estimate inst. HR
-    est_hr()
+    # Read in defined amount of time of data until end of file
+    buffer = sample_size
+    while (buffer<file_size):
+        data = read_binary(filename,offset=buffer,count_read=num_samples,init_flag=0)
+        ECG_data = data[0::2]
+        PP_data = data[1::2]
+        buffer = buffer + num_samples*sample_size
 
-    # Take in new inst. HR, drop old if it exists
-    # Add new inst. HR to 1 minute data, drop oldest if exists
-    # Add new inst. HR to 5 minute data, drop oldest if exists
-    # Add new inst. HR to 10 minute data, drop oldest if exists
-    # Check for too high / too low heart rate
-    proc_hr()
+        # Take in defined time of ECG and PP data at a time, estimate inst. HR
+        est_hr(ECG_data,PP_data,delta_t = (1/fs))
 
-    # Keep track of elapsed time
-    # Update inst. HR every 10 seconds
-    # Update 1, and 5 minute avg every 10 seconds if exists
-    # If alarm occurs, output 10 minute trace
-    gen_output()
+        # Take in new inst. HR, drop old if it exists
+        # Add new inst. HR to 1 minute data, drop oldest if exists
+        # Add new inst. HR to 5 minute data, drop oldest if exists
+        # Add new inst. HR to 10 minute data, drop oldest if exists
+        # Check for too high / too low heart rate
+        proc_hr()
+
+        # Keep track of elapsed time
+        # Update inst. HR every 10 seconds
+        # Update 1, and 5 minute avg every 10 seconds if exists
+        # If alarm occurs, output 10 minute trace
+        gen_output()
