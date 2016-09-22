4b4ecdf725c9a7823a2fd329fb3adfd42a54070e
Kyle Decker <kyle.decker@duke.edu>
2016-09-22 16:49:36 -0400

    Made significant changes to the main file, now reading in data every 1 second and displaying things better

--- heart_rate_monitor.py
+++ heart_rate_monitor.py
@@ -11,25 +11,39 @@ if __name__ == "__main__":
     import numpy as np
     import time
     import sys
-    # First attain necessary info (fs and size) from data                                                                                                                                              
-    data_info = read_binary(filename,offset=0,count_read=1,init_flag=1)
-    fs = data_info[0]
-    file_size = data_info[1]
+    import math
+    # First attain necessary info (fs and size) from data, as well as first 10 seconds                                                                                                                                              
+    data_info = read_binary(filename,offset=0,count_read=(1+2*fs*10),init_flag=1)
+    file_size = data_info[0]
+    fs = data_info[1]
+    start_data = data_info[2:]
 
-    # Define Amount of time to read in at once (10 seconds)
-    time_var = 10
+    # Define Amount of time to read in at once (1 second)
+    time_var = 1
     num_samples = fs*time_var
     sample_size = 2*2 #2 bytes per sample assuming uint16, 2 samples (1 ECG, 1 PP)
     HR_proc_data = np.zeros(num_samples*10*60/time_var) #Preallocate 10 minute trace
+    HR_proc_data[0:len(start_data)] = start_data
+    
+    ECG_data = HR_proc_data[0::2]
+    PP_data = HR_proc_data[1::2]
 
     # Read in defined amount of time of data until end of file
-    buffer = sample_size
+    buffer = sample_size + sample_size*fs*10
     total_elapsed_time = 0
     while (buffer<file_size):
-        start_time = time.time() 
+        start_time = time.time() #Start time for counter
         data = read_binary(filename,offset=buffer,count_read=num_samples,init_flag=0)
-        ECG_data = data[0::2]
-        PP_data = data[1::2]
+        
+        new_ecg = data[0::2]
+        new_pp = data[1::2]
+
+        # Replace old data with new in 10 second array
+        ECG_data = np.roll(ECG_data,len(new_ecg))
+        ECG_data[0:len(new_ecg)] = new_ecg
+        PP_data = np.roll(PP_data,len(new_pp))
+        PP_data[0:len(new_pp)] = new_pp
+        # Adjust the buffer to determine the offset
         buffer = buffer + num_samples*sample_size
 
         # Take in defined time of ECG and PP data at a time, estimate inst. HR
@@ -44,12 +58,16 @@ if __name__ == "__main__":
         # Keep track of elapsed time
         elapsed_time = time.time() - start_time
         total_elapsed_time = total_elapsed_time + elapsed_time
-        if (elapsed_time>10):
-            print("Elapsed Time: %d seconds \n" % total_elapsed_time)
-            print("Current Heart Rate = %d bpm \n" % inst_HR)
+        
+        if (elapsed_time<time_var):
+            time.sleep(time_var-elapsed_time) # Delay required time
+            os.system("clear")
+            print("Elapsed Time: %d seconds" % int(math.down(total_elapsed_time/10))*10 ) # Round 
+            print("Current Heart Rate = %d bpm" % inst_HR)
             if (total_elapsed_time>60):
-                print("1 Minute Average Heart Rate = %d bpm \n" % HR_avg_1min)
-                print("5 Minute Average Heart Rate = %d bpm \n" % HR_avg_5min)
-
+                print("1 Minute Average Heart Rate = %d bpm" % HR_avg_1min)
+                if (total_elapsed_time>(60*5)):
+                    print("5 Minute Average Heart Rate = %d bpm" % HR_avg_5min)
+            
 print("Reached the end of the data...")
 sys.exit()
