a1d1850463718446dae9069cc4706bcbd19814f0
Kyle Decker <kyle.decker@duke.edu>
2016-09-22 12:04:55 -0400

    Peak Detection is functional, passed unit test. This close #4

--- est_hr.py
+++ est_hr.py
@@ -8,6 +8,15 @@ def est_hr(ECG_data,PP_data,delta_t):
     """
 
     import numpy as np
-    import os
+    import peakutils
 
-    
+    # Find the peaks using a percentile threshold and min_dist
+    pt = 20 # Define the percentile to use
+    pk_dist = (60/400)/delta_t# Define the min distance between peaks - HR will not go over 400 bpm
+    signal_comb = ECG_data*PP_data
+    thresh_val = np.percentile(signal_comb,pt)
+    peak_ind = peakutils.indexes(signal_comb,thres=thresh_val,min_dist = pk_dist)
+    peak_separation = np.diff(peak_ind) # Find separation between peak indices
+
+    inst_HR = (1/(np.mean(peak_separation)*delta_t))*60 # Find Frequency and convert to bpm
+    return(inst_HR)
